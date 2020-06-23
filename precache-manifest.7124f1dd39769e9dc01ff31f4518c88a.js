b40c9bcf9dbc36f6e589ebe1c7c9aec6a58fb895
Josh Everett <josheverett93@gmail.com>
2020-06-23 12:23:20 -0500

    Updates

---
+++ precache-manifest.7124f1dd39769e9dc01ff31f4518c88a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b0e7da75548618736f697ee35f2c02a",
+    "url": "/employee-directory-react/index.html"
+  },
+  {
+    "revision": "a712b1513d01095081c2",
+    "url": "/employee-directory-react/static/css/main.0945deb8.chunk.css"
+  },
+  {
+    "revision": "c4d91621afac53dc9284",
+    "url": "/employee-directory-react/static/js/2.f37363f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory-react/static/js/2.f37363f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a712b1513d01095081c2",
+    "url": "/employee-directory-react/static/js/main.aeb04290.chunk.js"
+  },
+  {
+    "revision": "7c483b2024677cc109e7",
+    "url": "/employee-directory-react/static/js/runtime-main.0757c0fd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/employee-directory-react/precache-manifest.0cbf845cd6a84c6c94c0e20264cf3c85.js"
+  "/employee-directory-react/precache-manifest.7124f1dd39769e9dc01ff31f4518c88a.js"
 );
 
 self.addEventListener('message', (event) => {
